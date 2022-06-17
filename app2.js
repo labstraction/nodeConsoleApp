5962dd1cb68e9ae7dabf8fe2c006cc97b4e4d280
labstraction <45163263+labstraction@users.noreply.github.com>
2022-06-17 07:45:33 +0200

    parser 2

--- app2.js
+++ app2.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 
-const ParserV3 = require('./parserV3.js');
+//const ParserV3 = require('./parserV3.js');
 
-const fileToRead = "./assets/input/test6.csv";
-const fileToWrite = "./assets/output/output6.json";
+const fileToRead = "./assets/input/imperia.csv";
+const fileToWrite = "./assets/output/imperiaNew.csv";
 
 fs.readFile(fileToRead, 'utf8', fileReaderCallback)
 
@@ -14,11 +14,57 @@ function fileReaderCallback(err, data) {
     if(err){
         console.log(err);
     } else {
-        const arrayOfUsers = ParserV3.parseCSV(data, "#");
-        fs.writeFile(fileToWrite, JSON.stringify(arrayOfUsers), (err) => {
+        // const arrayOfUsers = ParserV3.parseCSV(data);
+        // const yUsers = arrayOfUsers.filter(user => filterCF(user));
+        const csv = filterCSV(data);
+        fs.writeFile(fileToWrite, csv, (err) => {
             if (err) {
                 console.log(err);
             }
         })
     }
+}
+
+function filterCF(user) {
+    const cf = user.CFlavoratore;
+    const yS = cf?.substring(6, 8)
+    const y = parseInt(yS);
+    if (y > 90 || y < 22) {
+        return true;
+    }
+    return false;
+}
+
+function toCSV(users){
+    let csv = "";
+    Object.keys(users[0]).forEach(key => {
+        csv += key + ",";
+    });
+    csv += "\n";
+    users.forEach(user => {
+        Object.keys(user).forEach(key => {
+            csv += user[key] + ",";
+        });
+        csv += "\n";
+    });
+    return csv.replaceAll("NaN", "");
+}
+
+
+function filterCSV(csv){
+    const lines = csv.split("\n");
+    const filteredLines = lines.filter((l, i) => {
+        if (i === 0) {
+            return true;
+        }
+        const worlds = l.split(",");
+        const cf = worlds[2];
+        const yS = cf?.substring(6, 8)
+        const y = parseInt(yS);
+        if (y <= 91 && y > 22) {
+            return true;
+        }
+        return false;
+    });
+    return filteredLines.join("\n");
 }
